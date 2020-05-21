ef11f0dbbc9b10dd93416b2899828f179eed3a81
Osama Mustafa <O.mustafaoracle@yahoo.com>
2020-05-21 14:25:54 +0300

    This is example for Create ATP  on OCI using Terraform
    This is example for Create ATP  on OCI using Terraform

---
+++ Create_ATP_DB/scripts/get_wallet.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+oci db autonomous-database generate-wallet --autonomous-database-id "${ATP_OCID}" --password "${ATP_WALLET_PASS}" --file ../resources/wallet.zip 
+
+cd ../resources
+unzip -o wallet.zip -d ./wallet/
+
+cd wallet
+export WALLET_DIR=`pwd`
+
+sed -i "s|DIRECTORY=\"[^\"]*|DIRECTORY=\"${WALLET_DIR}|" sqlnet.ora 
+
+export TNS_ADMIN=${WALLET_DIR}
+
+cd ~
